e006146ab7171c8d48ccbcc9d700b43714a6df61
Woongoh Cho <whdnddh@naver.com>
2023-07-31 12:52:15 +0900

    Seperate device manager

--- index.js
+++ index.js
@@ -7,36 +7,11 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
 
-const { deviceClasses } = require('./devices');
-
-const devices = [];
+const { Manager } = require('./manager');
 
 PORT = process.env.PORT || 3000;
 
-function saveDevices() {
-    console.log('Saving devices');
-    const data = devices.map((device) => device.config());
-    fs.writeFileSync('devices.json', JSON.stringify(data));
-}
-
-function loadDevices() {
-    console.log('Loading devices');
-    try {
-        const data = fs.readFileSync('devices.json');
-        const loadedDevices = JSON.parse(data);
-        loadedDevices.forEach((device) => {
-            const DeviceClass = deviceClasses[device.type];
-            if (DeviceClass) {
-                devices.push(new DeviceClass(device));
-            }
-        });
-    } catch (e) {
-        console.log('No devices file found');
-        saveDevices();
-    }
-}
-
-loadDevices();
+const manager = new Manager('devices.json');
 
 app.use(express.json());
 app.use('/viewer', express.static(path.join(__dirname, 'viewer')));
@@ -50,62 +25,43 @@ app.get('/api/ping', (req, res) => {
 });
 
 app.post('/api/add', (req, res) => {
-    const { type } = req.body;
-    DeviceClass = deviceClasses[type];
-    if (DeviceClass) {
-        devices.push(new DeviceClass(req.body));
-    } else {
-        res.status(400).send('Invalid type');
-        return;
+    try {
+        manager.addDevice(req.body.type, req.body);
+        res.send('ok');
+    } catch (e) {
+        res.status(400).send(e.message);
     }
-    saveDevices();
-    console.log('Added device', devices[devices.length - 1])
-    res.send('ok');
 });
 
 app.get('/api/list', (req, res) => {
-    const data = devices.map((device) => {
-        return {
-            id: device.id,
-            name: device.name,
-            type: device.type,
-        };
-    });
-    res.send(data);
+    res.send(manager.listDevices());
 });
 
 app.post('/api/remove', (req, res) => {
-    const { id } = req.body;
-    const index = devices.findIndex((device) => device.id === id);
-    if (index !== -1) {
-        const device = devices.splice(index, 1)[0];
-        device.remove?.();
-        saveDevices();
-        console.log('Removed device', id)
+    try {
+        manager.removeDevice(req.body.id);
+        res.send('ok');
+    } catch (e) {
+        res.status(400).send(e.message);
     }
-    res.send('ok');
 });
 
 app.get('/api/status/:id', (req, res) => {
     const { id } = req.params;
-    const device = devices.find((device) => device.id === id);
-    if (device) {
-        res.send(device.status(devices));
+    const status = manager.getDeviceStatus(id);
+    if (status) {
+        res.send(status);
     } else {
-        res.status(404).send('Not found');
+        res.status(404).send('Device not found');
     }
 });
 
 app.post('/api/update', (req, res) => {
-    const { id } = req.body;
-    const device = devices.find((device) => device.id === id);
-    if (device) {
-        device.update?.(req.body);
-        saveDevices();
-        console.log('Updated device', device.config())
+    try {
+        manager.updateDevice(req.body.id, req.body);
         res.send('ok');
-    } else {
-        res.status(404).send('Not found');
+    } catch (e) {
+        res.status(400).send(e.message);
     }
 });
 
---
+++ manager.js
@@ -0,0 +1,86 @@
+const fs = require('fs');
+
+const { deviceClasses } = require('./devices');
+
+class Manager {
+    constructor(savePath) {
+        this.devices = [];
+        this.savePath = savePath;
+        this.loadDevices();
+    }
+
+    saveDevices() {
+        console.log('[Manager] Saving devices');
+        const data = this.devices.map((device) => device.config());
+        fs.writeFileSync(this.savePath, JSON.stringify(data));
+    }
+
+    loadDevices() {
+        console.log('[Manager] Loading devices');
+        try {
+            const data = fs.readFileSync(this.savePath);
+            const loadedDevices = JSON.parse(data);
+            loadedDevices.forEach((device) => {
+                const DeviceClass = deviceClasses[device.type];
+                if (DeviceClass) {
+                    this.devices.push(new DeviceClass(device));
+                }
+            });
+        } catch (e) {
+            console.log('[Manager] No devices file found, creating new one');
+            this.saveDevices();
+        }
+    }
+
+    addDevice(type, config) {
+        const DeviceClass = deviceClasses[type];
+        if (DeviceClass) {
+            this.devices.push(new DeviceClass(config));
+        } else {
+            throw new Error('Invalid type');
+        }
+        this.saveDevices();
+    }
+
+    listDevices() {
+        return this.devices.map((device) => {
+            return {
+                id: device.id,
+                type: device.type,
+                status: device.status(),
+            };
+        });
+    }
+
+    removeDevice(id) {
+        const index = this.devices.findIndex((device) => device.id === id);
+        if (index !== -1) {
+            this.devices.splice(index, 1);
+        } else {
+            throw new Error('Device not found');
+        }
+        this.saveDevices();
+    }
+
+    getDeviceStatus(id) {
+        const device = this.devices.find((device) => device.id === id);
+        if (device) {
+            return device.status();
+        }
+        return null;
+    }
+
+    updateDevice(id, status) {
+        const device = this.devices.find((device) => device.id === id);
+        if (device) {
+            device.update(status);
+            this.saveDevices();
+        } else {
+            throw new Error('Device not found');
+        }
+    }
+}
+
+module.exports = {
+    Manager,
+};
